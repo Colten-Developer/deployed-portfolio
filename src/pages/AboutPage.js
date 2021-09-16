a3cb237fad3ff1bd2a3e4971471b3c02c9c803f4
Colten-Developer <ColtyG1@gmail.com>
2021-09-16 12:09:00 -0500

    edited about me page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -13,24 +13,16 @@ function AboutPage() {
           <section>
               <article>
                   <p>
-                  Before the pandemic I was an electrical engineering major at Texas A&M university. 
-                  At A&M I was a member of the A&M Judo team, even ranking silver in Dallas Invitational National Tournament!
-                  When the pandemic came through, I had to leave A&M and my team to return home. 
-                  <br></br>
-                  <br></br>
-                  While at A&M I excelled at my computer science classes.
-                  Needing to continue my education I found Thinkful, focusing on software engineering.
-                  I am excited about working with a team of people to make an idea become reality.
-                  <br></br>
-                  <br></br>
-                  This has been an amazing process to learn more about computer engineering. I intend to 
-                  use my education at Thinkful to become a software engineer, and hopefully start my own
-                  software engineering firm. 
+                    Hi! I am Colten a software engineer with experience in electrical engineering, customer service, SEO, and even management! I tend to see the bigger picture when it comes to projects. Coming from a management background I understand the need for things to be done in orderly, precise, and timely manner. 
                   </p>
-                  <p>Learned Technologies:</p>
-                  <p>React.js, Javascript, HTML, CSS, Python, Tkinter, Node.js, SQL, PostgresSQL, Express, NPM, Mocha and Chai</p>
-                  <p>Upcoming Technologies</p>
-                  <p>Wordpress, React Native</p>
+                  <br></br>
+                  <p>
+                    I started to code in High School, my first projects were simple games using Game Maker Studio and Udemy. In high school I eventually moved on to become the programming team captain of our state recognized robotics team. Once I moved on to college, I found I loved my computer science classes. Being able to process data and do advanced mathematics far faster than by hand was astonishing. Once the pandemic started, I found myself needing to find a way to continue my education online. That when I found the Thinkful program, this allowed me to easily switch my mindset from electrical engineering to full-stack development.
+                  </p>
+                  <br></br>
+                  <p>
+                    I understand the need for software to be put together cleanly from both the developer and client standpoint. When developing for the front-end I like to focus on the responsiveness of the application. This allows users seamless access to their applications from any device. When looking into back-end solutions I focus on the data. Handling data I find is the most important part of back-end development. Without proper data management nothing can be accomplished. 
+                  </p>
               </article>
           </section>
       </div>
